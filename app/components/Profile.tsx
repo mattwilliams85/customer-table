5f938234ad823ce8b1d868e20403ee30025c2727
Matthew Williams <mattwilliams85@gmail.com>
2025-03-13 14:42:04 -0700

    add customer update

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { FormEventHandler, useState } from "react";
 import { Customer } from "@_types/customer";
 
 interface PropTypes {
@@ -6,31 +9,83 @@ interface PropTypes {
 }
 
 const Profile = ({ customer, error }: PropTypes) => {
+  const [editableCustomer, setEditableCustomer] = useState(customer);
+  const [editMode, setEditMode] = useState<boolean>(false);
+
   if (error) return <p>Error: {error}</p>;
-  if (!customer) return <p>No customer found.</p>;
+  if (!editableCustomer) return <p>No customer found.</p>;
+
+  const handleChange = (field: string, value: string) => {
+    setEditableCustomer((prev) => {
+      if (!prev) return prev;
+      return {
+        ...prev,
+        attributes: {
+          ...prev.attributes,
+          [field]: value,
+        },
+      };
+    });
+  };
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!editableCustomer) return;
+    fetch(`http://localhost:1323/customers/${editableCustomer.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ customer: editableCustomer }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          console.error(data.error);
+        } else {
+          setEditMode(false);
+          setEditableCustomer(data.customer);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div>
-      <h1>Customer Info</h1>
+      <h1>{customer?.attributes.email}</h1>
+      <button className="border p-2" onClick={() => setEditMode(true)}>
+        Edit Mode
+      </button>
       <p>
-        <strong>ID:</strong> {customer.id}
+        <strong>ID:</strong> {editableCustomer.id}
       </p>
       <p>
-        <strong>Last Updated:</strong> {customer.last_updated}
+        <strong>Last Updated:</strong> {editableCustomer.last_updated}
       </p>
       <h2>Attributes</h2>
-      <p>
-        <strong>Company:</strong> {customer.attributes.company}
-      </p>
-      <p>
-        <strong>Created At:</strong> {customer.attributes.created_at}
-      </p>
-      <p>
-        <strong>Email:</strong> {customer.attributes.email}
-      </p>
-      <p>
-        <strong>Last Name:</strong> {customer.attributes.last_name}
-      </p>
+      <form onSubmit={handleOnSubmit}>
+        {Object.entries(editableCustomer.attributes).map(([key, value]) => (
+          <p key={key}>
+            <label className="block text-gray-500">
+              {key.replace("_", " ")}
+            </label>
+            {editMode ? (
+              <input
+                type="text"
+                className="border border-gray-300 p-2 rounded w-100"
+                value={value as string}
+                onChange={(e) => handleChange(key, e.target.value)}
+              />
+            ) : (
+              <span>{value as string}</span>
+            )}
+          </p>
+        ))}
+        <button type="submit" className="bg-blue-500 text-white p-2 mt-4">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
